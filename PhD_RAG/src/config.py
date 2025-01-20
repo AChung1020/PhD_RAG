15cc02dc7a68722159da5974e54cae9053e0560f
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-20 14:43:03 -0500

    chatbot in cli

--- PhD_RAG/src/config.py
+++ PhD_RAG/src/config.py
@@ -1,9 +1,13 @@
 # Global configurations
 import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Settings(BaseSettings):
     anthropic_api_key: str
+    api_url: str
     model_name: str = "claude-3-5-sonnet-20241022"
     max_tokens: int = 300
 
