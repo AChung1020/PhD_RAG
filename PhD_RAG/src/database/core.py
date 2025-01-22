3563446bf0c111acb53514e25944b1f879be2366
Andrew Chung <achun46@emory.edu>
2025-01-22 00:50:36 -0500

    poetry file update

--- PhD_RAG/src/database/core.py
+++ PhD_RAG/src/database/core.py
@@ -2,6 +2,7 @@ from pymilvus import MilvusClient
 
 client = MilvusClient("handbooks.db")
 
+
 def create_collection(collection_name):
     if client.has_collection(collection_name="LGS-csi-handbooks"):
         client.drop_collection(collection_name="LGS-csi-handbooks")
