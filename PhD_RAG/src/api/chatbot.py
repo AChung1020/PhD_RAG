e8003bc0a0abd64247c2427f64282e290659ea24
TAEEUN KIM <taeeunk1208@gmail.com>
2025-01-23 17:47:36 -0500

    connect contexts and query processing

--- PhD_RAG/src/api/chatbot.py
+++ PhD_RAG/src/api/chatbot.py
@@ -1,10 +1,17 @@
+from logging import getLogger
+from typing import Coroutine
+
 import anthropic
 import os
 from PhD_RAG.src.config import settings
 from fastapi import APIRouter, HTTPException, FastAPI
+
+from PhD_RAG.src.database.router import query_results
 from PhD_RAG.src.models import ChatRequest, ChatResponse
 
 router = APIRouter()
+logger = getLogger(__name__)
+
 
 client = anthropic.Anthropic(api_key=settings.claude_api_key)
 
@@ -18,21 +25,28 @@ async def get_response(request: ChatRequest):
         raise HTTPException(detail="Empty query", status_code=400)
 
     try:
-        response = await process_query(query)
+        retrieved_context = await query_results(query)
+        response = await process_query(query, retrieved_context)
     except Exception as e:
         raise HTTPException(detail=str(e), status_code=500)
     return ChatResponse(answer=response)
 
-async def process_query(query: str) -> str:
-    context = ""
+async def process_query(query: str, retrieved_context: dict) -> str:
+    context = "\n".join(doc.page_content for doc in retrieved_context['docs'])
+    max_context_length = 4000
+    if len(context) > max_context_length:
+        context = context[:max_context_length]
+        logger.warning("Context truncated due to length restrictions.")
+
     prompt = f"""
-        Context: {context}
-    
-        User Query: {query}
-    
-        Based on the provided context, answer the user's query accurately. If the context lacks sufficient information, state that clearly.
+    Context: {context}
+--------------------------------------
+    User Query: {query}
+--------------------------------------
+    Based on the provided context, answer the user's query accurately. If the context lacks sufficient information, state that clearly.
     """
 
+    print(f"Created Prompt: {prompt}")
     try:
         response = client.messages.create(
             model=settings.model_name,
